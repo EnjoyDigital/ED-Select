8cb451f5c6e6e1c91af65a84cad5c91e7b2caa10
unknown <glenntaylor@EDPCPS3.internal.local>
2014-06-26 12:42:02 +0100

    JS/CSS Files

---
+++ simpleSelect.js
@@ -0,0 +1,47 @@
+/**
+ * Creates an overlay for select boxes that can be styled
+ * how we want.
+ * @param  array options Options array
+ * @return mixed
+ *
+ * http://github.com/crot4lus/
+ */
+$.fn.simpleSelect = function(options){
+
+	var settings = $.extend({
+		'customClass': ''
+	}, options);
+
+	return this.each(function(){
+
+		var $select = $(this);
+
+		// Create a container for all the elements
+		var $selectWrapper = $('<div/>', {
+			'class': 'simpleSelect-box-wrapper'
+		});
+
+		// Create the box wrapper and put the select box inside it
+		$selectWrapper.insertAfter($select);
+		$select.appendTo($selectWrapper);
+
+		// Build up the structure of the custom select box
+		$selectBox = $('<div/>', {
+			'class': 'simpleSelect-box ' + settings.customClass
+		}).appendTo($selectWrapper);
+
+		$selectValue = $('<div/>', {
+			'class': 'box-value'
+		}).appendTo($selectBox).text( $('option:selected', $select).text() );
+
+		$selectArrow = $('<div/>', {
+			'class': 'box-arrow'
+		}).appendTo($selectBox);
+
+		// Create event triggers
+		$select.on('change', function(){
+			$selectValue.text( $('option:selected', $select).text() );
+		});
+
+	});
+};
