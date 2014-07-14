88b0066a5348719194b71b0bbe32377fb94eea76
Matthew-Wise <matthew.wise1@gmail.com>
2014-07-14 16:35:29 +0100

    Update simpleSelect.js

--- simpleSelect.js
+++ simpleSelect.js
@@ -4,38 +4,30 @@
  * @param  array options Options array
  * @return mixed
  */
-$.fn.simpleSelect = function(options) {
+$.fn.simpleSelect = function (options) {
     'use strict';
-	var settings = $.extend({
-		'customClass': ''
-	}, options);
+    var settings = $.extend({ 'customClass': '' }, options);
 
-	return this.each(function(){
+    return this.each(function () {
 
-		var $select = $(this);
+        var $select = $(this);
 
-		// Create a container for all the elements
-		var $selectWrapper = $('<div/>', { 'class': 'simpleSelect-box-wrapper' });
+        // Create a container for all the elements
+        var $selectWrapper = $('<div/>', { 'class': 'simpleSelect-box-wrapper' });
 
-		// Create the box wrapper and put the select box inside it
-		$selectWrapper.insertAfter($select);
-		$select.appendTo($selectWrapper);
+        // Create the box wrapper and put the select box inside it
+        $selectWrapper.insertAfter($select);
+        $select.appendTo($selectWrapper);
 
-		// Build up the structure of the custom select box
-		var $selectBox = $('<div/>', {
-			'class': 'simpleSelect-box ' + settings.customClass
-		}).appendTo($selectWrapper);
+        // Build up the structure of the custom select box
+        var $selectBox = $('<div/>', { 'class': 'simpleSelect-box ' + settings.customClass }).appendTo($selectWrapper);
 
-		var $selectValue = $('<div/>', {
-			'class': 'box-value'
-		}).appendTo($selectBox).text( $('option:selected', $select).text() );
+        var $selectValue = $('<div/>', { 'class': 'box-value' }).appendTo($selectBox).text($('option:selected', $select).text());
 
-		$('<div/>', {'class': 'box-arrow'}).appendTo($selectBox);
+        $('<div/>', { 'class': 'box-arrow' }).appendTo($selectBox);
 
-		// Create event triggers
-		$select.on('change', function () {
-		   $selectValue.text( $('option:selected', $select).text() );
-		});
+        // Create event triggers
+        $select.on('change', function () { $selectValue.text($('option:selected', $select).text()); });
 
-	});
+    });
 };
